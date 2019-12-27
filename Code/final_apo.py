eecf07ca82eebb5d8c27aa685bd9021f7c500f24
zijun-zhao <51279688+zijun-zhao@users.noreply.github.com>
2019-12-26 17:19:35 -1000

    code to run registration in parallel

--- Code/final_apo.py
+++ Code/final_apo.py
@@ -1,8 +1,54 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[1]:
 
 
+'''
+import shutil
+import numpy as np
+import os
+import pandas as pd
+import subprocess
+import numpy as np
+import time
+import multiprocessing
+from multiprocessing import Pool
+from functools import partial
+path = 'C:\\Users\\zzjun\OneDrive\\Documents\\WXWork\\1688850447423892\\Cache\\File\\2019-12\\somalist'
+
+
+# ### Move .apo to separate folders
+
+# In[3]:
+
+
+brain_list = []
+for i_apo in os.listdir(path):
+    (filename,extension) = os.path.splitext(i_apo)
+    (filename,extension) = os.path.splitext(filename)
+    #print(filename.split('_'))
+    if filename.split('_')[0] not in brain_list:
+        brain_list.append(filename.split('_')[0])
+
+for i_brain in brain_list:
+    folder = os.path.exists(os.path.join(path,i_brain))
+    if not folder:  
+        os.makedirs(os.path.join(path,i_brain))
+    for i_apo in os.listdir(path):
+        if i_apo.startswith(i_brain) and i_apo.endswith(".apo"):
+            oldname = os.path.join(path,i_apo)
+            newname = os.path.join(os.path.join(path,i_brain),i_apo)
+            shutil.move(oldname, newname)
+
+
+
+# In[ ]:
+
+
+
+
+'''
 import pandas as pd
 import subprocess
 import shutil
@@ -12,15 +58,10 @@ import time
 import multiprocessing
 from multiprocessing import Pool
 from functools import partial
-import pandas as pd
-import subprocess
-import shutil
-import numpy as np
-import os
-import time
-import multiprocessing
-from multiprocessing import Pool
-from functools import partial
+
+
+
+
 
 import random
 import ast
@@ -31,21 +72,22 @@ import matplotlib.pyplot as plt
 import multiprocessing
 import time
 
-path = ''
+path = 'F:\\Parallel_apo'
 
 
-
-def single_brain_log(brain_regis, path, input_brainID):
+def single_apo(brain_regis, path, input_brainID):
+    affine_size = 0
+    
     exist_ccfbrain = os.path.exists(os.path.join(path, 'average_template_25_u8_xpad.v3draw'))
     if not exist_ccfbrain:
         print('Make sure the CCF standard brain template exists.')
     path_ccfbrain = os.path.join(path, 'average_template_25_u8_xpad.v3draw')
 
     ##
-    exist_exe_affine = os.path.exists(os.path.join(os.path.join(path, 'second_affine'), 'main_warp_from_affine.exe'))
+    exist_exe_affine = os.path.exists(os.path.join(os.path.join(path, 'second_affine'), 'affine_apo.exe'))
     if not exist_exe_affine:
         print('Make sure the .exe to perform affinement exists')
-    path_exe_affine = os.path.join(os.path.join(path, 'second_affine'), 'main_warp_from_affine.exe')
+    path_exe_affine = os.path.join(os.path.join(path, 'second_affine'), 'affine_apo.exe')
 
     exist_manaulM = os.path.exists(os.path.join(os.path.join(path, 'second_affine'), 'Manual_marker'))
     if not exist_manaulM:
@@ -57,21 +99,16 @@ def single_brain_log(brain_regis, path, input_brainID):
         print('Make sure the folder containing marker inside CCF exists.')
     path_ccfM = os.path.join(os.path.join(path, 'second_affine'), 'CCF_marker')
 
-    exist_stripmove = os.path.exists(os.path.join(os.path.join(path, 'second_affine'), 'stripMove'))
-    if not exist_stripmove:
-        print('Make sure the folder containing CCF brain of removing strips exists.')
-    path_stripm = os.path.join(os.path.join(path, 'second_affine'), 'stripMove')
 
-    ##
-    exist_exe_warp = os.path.exists(os.path.join(os.path.join(path, 'third_warp_swc'), 'main_warp_from_df.exe'))
+    exist_exe_warp = os.path.exists(os.path.join(os.path.join(path, 'third_warp_swc'), 'main_warp_apo_from_df.exe'))
     if not exist_exe_warp:
         print('Make sure the .exe to perform warp exists')
-    path_exe_warp = os.path.join(os.path.join(path, 'third_warp_swc'), 'main_warp_from_df.exe')
+    path_exe_warp = os.path.join(os.path.join(path, 'third_warp_swc'), 'main_warp_apo_from_df.exe')
 
     exist_afBrain = os.path.exists(os.path.join(os.path.join(path, 'third_warp_swc'), 'affined_brain'))
     if not exist_afBrain:
         print('Make sure the folder containing affined brains exists.')
-    path_norm_brain = os.path.join(os.path.join(path, 'third_warp_swc'), 'affined_norm_brain')
+    path_af_brain = os.path.join(os.path.join(path, 'third_warp_swc'), 'affined_brain')
 
     exist_auto_brain_m = os.path.exists(os.path.join(os.path.join(path, 'third_warp_swc'), 'brain_auto_marker'))
     if not exist_auto_brain_m:
@@ -87,17 +124,14 @@ def single_brain_log(brain_regis, path, input_brainID):
     if not exist_ssd:
         print('Make sure the folder containing grid. swc generated using SSD algorithm.')
     path_ssdgrid = os.path.join(os.path.join(path, 'third_warp_swc'), 'ssd_grid')
-
-
-
-
     print(" Process brain %s" % (input_brainID))
+
     folder_ori = os.path.join(os.path.join(path, input_brainID), 'ori')
     exist_ori = os.path.exists(folder_ori)
     if not exist_ori:
         os.makedirs(folder_ori)
     for iter_check in os.listdir(os.path.join(path, input_brainID)):
-        if iter_check.endswith(("swc", "SWC")):
+        if iter_check.endswith(("ano.apo")):
             shutil.move(os.path.join(os.path.join(path, input_brainID), iter_check), folder_ori)
             print('Move ' + str(iter_check) + ' to folder ori')
     input_brainID = input_brainID
@@ -112,57 +146,48 @@ def single_brain_log(brain_regis, path, input_brainID):
         print('Creat new folder ' + str(folder_resample))
         os.makedirs(folder_resample)
 
-    folder_stps = os.path.join(os.path.join(path, input_brainID), 'stps')
-    exist_stps = os.path.exists(folder_stps)
-    if not exist_stps:
-        print('Creat new folder ' + str(folder_stps))
-        os.makedirs(folder_stps)
-
-    print('\n-----DOWNSAMPLE-------')
+    folder_result = os.path.join(os.path.join(path, input_brainID), 'result')
+    exist_result = os.path.exists(folder_result)
+    if not exist_result:
+        print('Creat new folder ' + str(folder_result))
+        os.makedirs(folder_result)
         # specific downsample size for the input_brainID
+    print('\n-----DOWNSAMPLE-------')
     x_ds_size = brain_regis.loc[int(input_brainID), 'x_downsample']
     y_ds_size = brain_regis.loc[int(input_brainID), 'y_downsample']
     z_ds_size = brain_regis.loc[int(input_brainID), 'z_downsample']
 
     for iter_swc in os.listdir(folder_ori):
-        if not iter_swc.endswith(("swc", "SWC")):
+        if not iter_swc.endswith(("ano.apo")):
             continue
         (neuron_info, extension) = os.path.splitext(iter_swc)
         print('Neuron information: ' + str(neuron_info))
+        
         path_old = os.path.join(folder_ori, iter_swc)
         path_new = os.path.join(folder_resample, neuron_info) + '.' + 'x' + str(int(round(x_ds_size, 0))) + 'y' + str(
-            int(round(y_ds_size, 0))) + 'z' + str(int(round(z_ds_size, 0))) + str(extension)
-        n_skip = 0
-        with open(path_old, "r") as f:
-            for line in f.readlines():
-                line = line.strip()
-                if line.startswith("#"):
-                    n_skip += 1
-                else:
-                    break
-        names = ["##n", "type", "x", "y", "z", "r", "parent"]
-        swc = pd.read_csv(path_old, index_col=0, skiprows=n_skip, sep=" ",
-                          usecols=[0, 1, 2, 3, 4, 5, 6],
-                          names=names
-                          )
-        swc.loc[:, 'x'] = swc.loc[:, 'x'] / round(x_ds_size, 1)
-        swc.loc[:, 'y'] = swc.loc[:, 'y'] / round(y_ds_size, 1)
-        swc.loc[:, 'z'] = swc.loc[:, 'z'] / round(z_ds_size, 1)
-        swc = swc.round({'x': 4, 'y': 4, 'z': 4})
-        assert (max(swc.loc[:, 'x'])) < brain_regis.loc[int(input_brainID), 'x_after'], " x has exceeds the boundary"
-        assert (max(swc.loc[:, 'y'])) < brain_regis.loc[int(input_brainID), 'y_after'], " y has exceeds the boundary"
-        assert (max(swc.loc[:, 'z'])) < brain_regis.loc[int(input_brainID), 'z_after'], " z has exceeds the boundary"
-
-        swc.to_csv(path_new, sep=" ")
+            int(round(y_ds_size, 0))) + 'z' + str(int(round(z_ds_size, 0))) + '.s.ano.apo'
 
         print('Saving current file to ' + str(path_new))
+        file = open(path_old, 'r')
+        f = open(path_new, 'w+')
+        lines = file.readlines()
+        for line in lines:
+            S = line.strip().split(',')
+            if S[0] == '##n':
+                f.write(line)
+                continue
+            z = float(S[4]) / z_ds_size
+            z = round(z, 3)
+            x = float(S[5]) / x_ds_size
+            x = round(x, 3)
+            y = float(S[6]) / y_ds_size
+            y = round(y, 3)
+            f.write('{}, ,   {},,{},{},{},{},{},{},{},{},,,,{},{},{}\n'.format(S[0], S[2], z, x, y, S[7], S[8], S[9], S[10],
+                                                                               S[11], S[15], S[16], S[17]))
 
     print('\n-----Affinement-------')
     print('Obtaining three files for brain ' + str(input_brainID) + ' for affinement:')
-    for iter_s in os.listdir(path_stripm):
-        if iter_s.startswith(input_brainID):
-            path_rm_strip = os.path.join(path_stripm, iter_s)
-            print('\tObtained .v3draw of strips-removed version for brain ' + str(input_brainID))
+    path_manual_m = ''
     # Record manually-labeled file for specific brain
     # Record size
     for iter_m in os.listdir(path_manaulM):
@@ -178,85 +203,91 @@ def single_brain_log(brain_regis, path, input_brainID):
             path_ccf_m = os.path.join(path_ccfM, iter_c)
             print('\tObtained standard-labelled marker file for brain ' + str(input_brainID))
 
+
     if os.path.exists(path_exe_affine) != 1:
         print('Make sure the .exe to perform affinement exists')
-    if os.path.exists(path_ccfbrain) != 1:
-        print('Make sure the CCF standard brain template exists.')
-    if os.path.exists(path_rm_strip) != 1:
-        print('Make sure the folder containing CCF brain of removing strips exists.')
+    # if os.path.exists(path_ccfbrain) != 1:
+    #     print('Make sure the CCF standard brain template exists.')
+
     if os.path.exists(path_ccf_m) != 1:
         print('Make sure the folder containing marker inside CCF exists.')
+    if os.path.exists(path_ccf_m) != 1:
+        print('Make sure the folder containing all manually-labelled ccf marker files exists.')
     if os.path.exists(path_manual_m) != 1:
-        print('Make sure the folder containing all manually-labelled marker files exists.')
-    if os.path.exists(path_manual_m) != 1:
-        print('Make sure the folder containing all manually-labelled marker files exists.')
-    assert os.path.exists(path_exe_affine) == 1 and os.path.exists(path_ccfbrain) == 1 and os.path.exists(
-        path_rm_strip) == 1 and os.path.exists(path_ccf_m) == 1 and os.path.exists(
-        path_manual_m) == 1 and os.path.exists(path_manual_m) == 1, "Check the corresponding document"
-    path_op_affine = os.path.join(folder_affine, '%%~ni_affine.swc')
-    with open(os.path.join(os.path.join(path, input_brainID), 'affine_swc.bat'), 'w') as OPATH:
+        print('Make sure the folder containing all manually-labelled marker files of brain '+str(input_brainID)+' exists.')
+        
+    if not  os.path.exists(path_exe_affine) == 1  and os.path.exists(path_ccf_m) == 1 and os.path.exists(
+        path_manual_m) == 1:
+        print("Check the corresponding document of brain " + str(input_brainID))
+        return
+    path_op_affine = os.path.join(folder_affine, '%%~ni.affine.apo')
+    with open(os.path.join(os.path.join(path, input_brainID), 'apo_affine.bat'), 'w') as OPATH:
         OPATH.writelines(['\n',
                           'set input_path=' + str(folder_resample),
                           '\n',
-                          'for /r %input_path% %%i in (*.swc) do (',
+                          'for /r %input_path% %%i in (*.apo) do (',
                           '\n',
-                          str(path_exe_affine) + ' -t ' + str(path_ccfbrain) + ' -s ' + str(path_rm_strip) +
-                          ' -T ' + str(path_ccf_m) + ' -S ' + str(path_manual_m) + '  -w %%i -o ' + str(path_op_affine),
+                          str(path_exe_affine) + ' -t ' + str(path_ccf_m) + ' -s ' + str(path_manual_m) + ' -S %%i -a ' 
+                           + str(path_op_affine),
                           '\n',
                           '\n',
                           ')',
-                          '\n'])
-    filepath_Aff = os.path.join(os.path.join(path, input_brainID), 'affine_swc.bat')
-    print('Run code at ' + str(filepath_Aff))
+                          ])
+    filepath_Aff = os.path.join(os.path.join(path, input_brainID), 'apo_affine.bat')
+    print('Run code at '+ str(filepath_Aff))
     os.system(filepath_Aff)
-    assert len(os.listdir(folder_affine)) == len(
-        os.listdir(folder_resample)), "Note that affined swc is not equal to resampled swc!"
+    if not len(os.listdir(folder_affine)) == len(
+        os.listdir(folder_resample)):
+        print("\n\t*****************Note that affined swc is not equal to resampled swc for brain "+str(input_brainID))
     print('Have finished affinement')
 
     print('-----WRAP-----')
     subtime = time.time()
     print('Obtaining four files for brain ' + str(input_brainID) + ' for warp:')
-    for iter_d in os.listdir(path_norm_brain):
+    path_af_input_brainID = ''
+    
+    for iter_d in os.listdir(path_af_brain):
         if iter_d.startswith(input_brainID):
-            path_af_norm_brainID = os.path.join(path_norm_brain, iter_d)
+            path_af_input_brainID = os.path.join(path_af_brain, iter_d)
             print('\tObtained affined version for brain ' + str(input_brainID))
+    path_ssd = ''
     for iter_ssd in os.listdir(path_ssdgrid):
         if iter_ssd.startswith(input_brainID):
             path_ssd = os.path.join(path_ssdgrid, iter_ssd)
             print('\tObtained SSD result for brain ' + str(input_brainID))
+    path_ccfAT_m = ''
     for iter_cm in os.listdir(path_autoCCF_m):
         if iter_cm.startswith(input_brainID):
             path_ccfAT_m = os.path.join(path_autoCCF_m, iter_cm)
             print('\tObtained auto integrated marker for CCF brain ')
+    path_brainAT_m = ''
     for iter_bm in os.listdir(path_autoBrain_m):
         if iter_bm.startswith(input_brainID):
             path_brainAT_m = os.path.join(path_autoBrain_m, iter_bm)
             print('\tObtained auto integrated marker for brain ' + str(input_brainID))
-
-    path_op_warp = os.path.join(folder_stps, '%%~ni_stps.swc')
-    #path_op_v3draw = os.path.join(os.path.join(path, input_brainID), 'stps')
+    
+    path_op_warp = os.path.join(folder_result, '%%~ni.warp.apo')
     # directory = 'C:/'
-    with open(os.path.join(os.path.join(path, input_brainID), 'warp_swc.bat'), 'w') as OPATH_2:
+    with open(os.path.join(os.path.join(path, input_brainID), 'apo_warp_main.bat'), 'w') as OPATH_2:
         OPATH_2.writelines(['\n',
-                            'set input_path=' + str(folder_affine),
-                            '\n',
-                            'for /r %input_path% %%i in (*.swc) do (',
-                            '\n',
-                            str(path_exe_warp) + ' -t ' + str(path_ccfbrain) + ' -s ' + str(path_af_norm_brainID) +
-                            ' -g ' + str(path_ssd) + ' -T ' + str(path_ccfAT_m) + ' -S ' + str(
-                                path_brainAT_m) + ' -w %%i  -o ' + str(path_op_warp),
-                            '\n',
-                            '\n',
-                            ')',
-                            '\n'])
-    filepath_Warp = os.path.join(os.path.join(path, input_brainID), 'warp_swc.bat')
-    print('Run code at ' + str(filepath_Warp))
+                          'set input_path=' + str(folder_affine),
+                          '\n',
+                          'for /r %input_path% %%i in (*.apo) do (',
+                          '\n',
+                          str(path_exe_warp) + ' -s '  + str(path_af_input_brainID) +
+                          ' -w %%i  -a ' + str(path_ssd) + ' -T ' + str(path_ccfAT_m) + ' -S ' + str(
+                              path_brainAT_m) + ' -f ' + str(path_op_warp),
+                          '\n',
+                          '\n',
+                          ')',
+                          '\n'])
+    filepath_Warp = os.path.join(os.path.join(path, input_brainID), 'apo_warp_main.bat')
+    print('Run code at '+ str(filepath_Warp))
     os.system(filepath_Warp)
     print(" Process brain %s Finished." % (input_brainID))
     sub_elapsed = time.time() - subtime
-    print(" Time used for warpping brain " + str(input_brainID) + ' is %.2f ' % sub_elapsed)
+    print(" Time used for warpping brain " + str(input_brainID)+ ' is %.2f ' % sub_elapsed)
     print('Have finished Warp')
-
 
 if __name__ == '__main__':
     start = time.time()
@@ -285,15 +316,12 @@ if __name__ == '__main__':
     print(brain_folder_list)
     cores = int(multiprocessing.cpu_count() * 0.6)  # multiprocessing.cpu_count()
     pool = multiprocessing.Pool(processes=cores)
-
     for iter_bb in brain_folder_list:
-        pool.apply_async(single_brain_log, (brain_regis, path, iter_bb))
-        # single_brain_log(brain_regis,path,iter_bb)
+        pool.apply_async(single_apo, (brain_regis,path,iter_bb))
+        #single_apo(brain_regis,path,iter_bb)
     pool.close()
     pool.join()
     elapsed = (time.time() - start)
     print("Used %.2f to run all the brian" % elapsed)
 
-
-
-
+    
